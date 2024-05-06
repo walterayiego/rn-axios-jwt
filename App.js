5a74f508eab8ff77ba53ddf4b5db1dd4ab78c9db
walterayiego <walterayiego@gmail.com>
2024-05-06 18:26:52 +0300

    Clean up and restructure

--- App.js
+++ App.js
@@ -1,10 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { api, serv } from "./api/api";
+import { useState } from "react";
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [loggedUser, setLoggedUser] = useState(null);
+
+  const getUser = async () => {
+    if (!user?.accessToken) return;
+    try {
+      const response = await serv.get("/user-logged-in", {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response.data);
+      setLoggedUser(response.data.username);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const response = await api.get("/users");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logIn = async () => {
+    try {
+      const res = await api.post("/login", {
+        username: "Jane",
+      });
+      setUser(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getNewToken = async () => {
+    try {
+      const res = await api.post("/getnewToken", {
+        token: user.refreshToken,
+      });
+      setUser((prev) => ({ ...prev, accessToken: res.data.accessToken }));
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={getUser} style={styles.btn}>
+        <Text>Get user Logged</Text>
+      </TouchableOpacity>
+
+      <Text> User Logged : {loggedUser ? loggedUser : "No user"}</Text>
+
+      <TouchableOpacity onPress={getUsers} style={styles.btn}>
+        <Text>Get Users</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={logIn} style={styles.btn}>
+        <Text className="bg-black">Log In</Text>
+      </TouchableOpacity>
+      <View className="">
+        <Text> AccessToken : {user ? user.accessToken : "No user"}</Text>
+        <Text> RefreshToken : {user ? user.refreshToken : "No user"}</Text>
+      </View>
+
+      <TouchableOpacity onPress={getNewToken} style={styles.btn}>
+        <Text className="bg-black">Get New Token</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +87,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    width: 300,
+    borderBlockColor: "black",
+    borderWidth: 1,
+    margin: 10,
   },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./App.{js,jsx,ts,tsx}"],
+  presets: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
